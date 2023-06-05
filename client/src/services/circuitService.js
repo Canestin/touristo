24ec1aef9739f7cb5eec89a6a92d725829d1a134
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-06-05 22:43:50 +0200

    refactor: delete client side

--- client/src/services/circuitService.js
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-
-export const getCircuitById = (uuid) => api.get(`/circuits/${uuid}`);
-export const getCircuits = () => api.get("/circuits");
-export const createCircuit = (search) => api.get(`/circuits?${search}`);
-
-// department=75&numberOfDays=3&numberOfSitePerDay=3&latitude=48.877059&longitude=2.329685&city=Paris
--- client/src/services/siteService.js
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-
-const siteService = {
-  getSites: () => api.get("/sites"),
-  getSiteById: (siteId) => api.get(`/sites/${siteId}`),
-};
-
-export default siteService;
