e86d11b20391ef15d6554c737d3fb103554f32a0
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-06-06 15:31:26 +0200

    chore(client): add prod api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://touristo.herokuapp.com",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
